3e11328cea76186fee8045d1e0a67d1aa3ef323c
kutmicro2017 <32184745+kutmicro2017@users.noreply.github.com>
2017-10-10 09:19:31 +0900

    Update real.ino

--- real.ino
+++ real.ino
@@ -36,74 +36,74 @@ void loop() {
   // 사거리 신호등을 상 1 하 2 좌 3 우 4 라 하면 1번 초록불 -> 2번 -> 3번 -> 4번 -> 반복
   //녹색 -> 노랑 -> 빨강 -> 녹색->무한루프
   //신호가 초록일때 보행자 버튼 (3초 후에 노랑 -> 빨강)
-  digitalWrite(LED2_R, HIGH); digitalWrite(LED3_R, HIGH); digitalWrite(LED4_R, HIGH); 
-  digitalWrite(LED1_R, LOW); digitalWrite(LED1_G, HIGH); //위쪽 신호등만 초록색
-  delay(40000); //40초간 켜줌
-  digitalWrite(LED1_G, LOW);
-  for(int delaytime=0; delaytime <10; delaytime++){// 초록 -> 빨강 될때 황색으로 경고
-    digitalWrite(LED1_Y, HIGH);
-    delay(500);  
-    digitalWrite(LED1_Y, LOW);
-    delay(500); 
+digitalWrite(LED2_R, HIGH); digitalWrite(LED3_R, HIGH); digitalWrite(LED4_R, HIGH); 
+digitalWrite(LED1_R, LOW); digitalWrite(LED1_G, HIGH); //위쪽 신호등만 초록색
+delay(40000); //40초간 켜줌
+digitalWrite(LED1_G, LOW);
+for(int delaytime=0; delaytime <10; delaytime++){// 초록 -> 빨강 될때 황색으로 경고
+digitalWrite(LED1_Y, HIGH);
+delay(500);  
+digitalWrite(LED1_Y, LOW);
+delay(500); 
   } 
+digitalWrite(LED1_G, LOW);
 // ---------------------------------------------------1번(상)만 초록
-  digitalWrite(LED1_R, HIGH); 
-  digitalWrite(LED2_R, LOW); digitalWrite(LED2_G, HIGH); //아래쪽 신호등만 초록색
-  unsigned long previousMillis = millis();
-  while(millis() - previousMillis <= interval) { //현재 시각과 이전 시각의 차이가 40초가 될 때까지 반복
-    //시간 지연 동안에 다른 동작을 해야하므로 delay함수는 사용 못함
-    if(state == false) { //스위치가 눌렸으면
-      
-      delay(3000); //3초간 대기
-      digitalWrite(LED2_G, LOW); //초록불을 꺼준다
-      for(int delaytime=0; delaytime <10; delaytime++){ //10초간
-        digitalWrite(LED2_Y, HIGH); //노란 신호가 들어옴
-        delay(500);
-        digitalWrite(LED2_Y, LOW); //노란 신호를 끔
-        delay(500); //1초 간격으로 깜빡임
-      }
-      digitalWrite(LED2_R, HIGH); //빨간 신호를 켠다
-      delay(20000);
-      state = true; //동작이 끝났으니 변수를 원래 상태로 돌려줌
-      digitalWrite(LED2_R, LOW);
-      digitalWrite(LED2_G, HIGH); //다시 초록 신호를 켜준다
-      
-    }
-  }
-  delay(40000);
-  interval = 40000; //인터벌 40초로 초기화
-  digitalWrite(LED2_G, LOW);
-  for(int delaytime=0; delaytime <10; delaytime++){
-    digitalWrite(LED2_Y, HIGH);
-    delay(500);  
-    digitalWrite(LED2_Y, LOW);
-    delay(500); // 초록 -> 빨강 될때 황색으로 경고
+digitalWrite(LED1_R, HIGH); 
+digitalWrite(LED2_R, LOW); digitalWrite(LED2_G, HIGH); //아래쪽 신호등만 초록색
+unsigned long previousMillis = millis();
+while(millis() - previousMillis <= interval) { //현재 시각과 이전 시각의 차이가 40초가 될 때까지 반복
+  //시간 지연 동안에 다른 동작을 해야하므로 delay함수는 사용 못함
+ if(state == false) { //스위치가 눌렸으면
+    interval -= (millis()-previousMillis); //2번 초록불의 남은 시간을 인터벌에 새로 저장
+    delay(3000); //3초간 대기
+    digitalWrite(LED2_G, LOW); //초록불을 꺼준다
+    for(int delaytime=0; delaytime <10; delaytime++){ //10초간
+    digitalWrite(LED2_Y, HIGH); //노란 신호가 들어옴
+    delay(500);
+    digitalWrite(LED2_Y, LOW); //노란 신호를 끔
+    delay(500); //1초 간격으로 깜빡임
+    } 
+    digitalWrite(LED2_R, HIGH); //빨간 신호를 켠다
+    delay(20000);
+    state = true; //동작이 끝났으니 변수를 원래 상태로 돌려줌
+    digitalWrite(LED2_R, LOW);
+    digitalWrite(LED2_G, HIGH); //다시 초록 신호를 켜준다
+    previousMillis = millis(); //현재시각 저장
+ }
+}
+interval = 40000; //인터벌 40초로 초기화
+digitalWrite(LED2_G, LOW);
+for(int delaytime=0; delaytime <10; delaytime++){
+digitalWrite(LED2_Y, HIGH);
+delay(500);  
+digitalWrite(LED2_Y, LOW);
+delay(500); // 초록 -> 빨강 될때 황색으로 경고
   } 
-  digitalWrite(LED2_G, LOW);
+digitalWrite(LED2_G, LOW);
 // ---------------------------------------------------2번(하) 초록 보행자신호 넣어주기
-  digitalWrite(LED2_R, HIGH); 
-  digitalWrite(LED3_R, LOW); digitalWrite(LED3_G, HIGH); //위쪽 신호등만 초록색
-  delay(40000); //40초간 켜줌
-  digitalWrite(LED3_G, LOW);
-  for(int delaytime=0; delaytime <10; delaytime++){
-    digitalWrite(LED3_Y, HIGH);
-    delay(500);  
-    digitalWrite(LED3_Y, LOW);
-    delay(500); // 초록 -> 빨강 될때 황색으로 경고
-  }
-  digitalWrite(LED3_G, LOW);
+digitalWrite(LED2_R, HIGH); 
+digitalWrite(LED3_R, LOW); digitalWrite(LED3_G, HIGH); //위쪽 신호등만 초록색
+delay(40000); //40초간 켜줌
+digitalWrite(LED3_G, LOW);
+for(int delaytime=0; delaytime <10; delaytime++){
+digitalWrite(LED3_Y, HIGH);
+delay(500);  
+digitalWrite(LED3_Y, LOW);
+delay(500); // 초록 -> 빨강 될때 황색으로 경고
+}
+digitalWrite(LED3_G, LOW);
 // ---------------------------------------------------3번(좌) 초록
-  digitalWrite(LED3_R, HIGH); 
-  digitalWrite(LED4_R, LOW); digitalWrite(LED4_G, HIGH); //위쪽 신호등만 초록색
-  delay(40000); //40초간 켜줌
-  digitalWrite(LED4_G, LOW);
-  for(int delaytime=0; delaytime <10; delaytime++){
-    digitalWrite(LED4_Y, HIGH);
-    delay(500);  
-    digitalWrite(LED4_Y, LOW);
-    delay(500); // 초록 -> 빨강 될때 황색으로 경고
-  }
-  digitalWrite(LED4_G, LOW);
+digitalWrite(LED3_R, HIGH); 
+digitalWrite(LED4_R, LOW); digitalWrite(LED4_G, HIGH); //위쪽 신호등만 초록색
+delay(40000); //40초간 켜줌
+digitalWrite(LED4_G, LOW);
+for(int delaytime=0; delaytime <10; delaytime++){
+digitalWrite(LED4_Y, HIGH);
+delay(500);  
+digitalWrite(LED4_Y, LOW);
+delay(500); // 초록 -> 빨강 될때 황색으로 경고
+}
+digitalWrite(LED4_G, LOW);
 // ---------------------------------------------------4번(우) 초록
 }
 
